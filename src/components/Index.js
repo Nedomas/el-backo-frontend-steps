2f68a11a3569250845b6cafea607d8d611ffd321
Domas <domas.bitvinskas@me.com>
2019-01-06 13:19:57 +0200

    6 - Add Link in Index component to Singleplayer route

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default () => (
   <div>
-    In Index route here
+    <div>
+      In Index route here
+    </div>
+
+    <Link to='/singleplayer/new'>
+      Singleplayer
+    </Link>
   </div>
 )
