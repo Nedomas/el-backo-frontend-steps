cbec4182653af4e6cb4b7efad83abc7789681da2
Domas <domas.bitvinskas@me.com>
2019-02-10 23:05:54 +0200

    2.6.1 - Use createGame mutation and print results to the screen

--- src/components/SingleplayerPlayerNames/index.js
+++ src/components/SingleplayerPlayerNames/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import _ from 'lodash'
 import { Formik, Field } from 'formik'
+import { Mutation } from 'react-apollo'
 import MenuContainer from '../MenuContainer'
 import Button from '../Button'
 import initialValues from './lib/initialValues'
+import createGameMutation from './lib/createGameMutation'
 import styles from './index.module.css'
 
 export default ({
@@ -14,29 +16,36 @@ export default ({
   },
 }) => (
   <MenuContainer>
-    <Formik
-      initialValues={initialValues(playersCount)}
-      onSubmit={(variables) => (
-        console.log('Form submitted with:', variables)
-      )}
-    >
-      {({ values: { players }, handleSubmit, errors }) => (
-        <form onSubmit={handleSubmit}>
-          {_.map(players, ({ number }, index) => (
-            <div key={index}>
-              <Field
-                name={`players.${index}.name`}
-                className={styles.field}
-                placeholder={`Player ${index + 1} name`}
-              />
-            </div>
-          ))}
+    <Mutation mutation={createGameMutation}>
+      {(createGame, { data }) => (
+        <Formik
+          initialValues={initialValues(playersCount)}
+          onSubmit={async (variables) => {
+            console.log('Form submitted with:', variables)
+            createGame({ variables })
+          }}
+        >
+          {({ values: { players }, handleSubmit, errors }) => (
+            <form onSubmit={handleSubmit}>
+              {_.map(players, ({ number }, index) => (
+                <div key={index}>
+                  <Field
+                    name={`players.${index}.name`}
+                    className={styles.field}
+                    placeholder={`Player ${index + 1} name`}
+                  />
+                </div>
+              ))}
 
-          <Button component='button' type='submit'>
-            Start game
-          </Button>
-        </form>
+              <Button component='button' type='submit'>
+                Start game
+              </Button>
+
+              {JSON.stringify(data)}
+            </form>
+          )}
+        </Formik>
       )}
-    </Formik>
+    </Mutation>
   </MenuContainer>
 )
