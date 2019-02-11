f4a2fb22eceff83550e0f1419d8f148089759b09
Domas <domas.bitvinskas@me.com>
2019-02-11 14:13:03 +0200

    3.5.4 - Position Board spaces via svg coordinates

--- src/components/Game/Board/index.js
+++ src/components/Game/Board/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import _ from 'lodash'
+import position from './lib/position'
 import styles from './index.module.css'
 
 export default ({
@@ -9,9 +10,13 @@ export default ({
     },
   },
 }) => (
-  <div>
+  <div className={styles.container}>
     {_.map(spaces, (space) => (
-      <div key={space.id} className={styles.space} />
+      <div
+        key={space.id}
+        className={styles.space}
+        style={{ ...position(space) }}
+      />
     ))}
   </div>
 )
